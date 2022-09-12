f98753659be3e1dcb94b8b302eaf8416a5472294
Gil Creque <me@gilcreque.com>
2022-09-12 02:14:07 +0000

    Switched to using .env

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,10 +8,10 @@ const Hero = () => (
             <img src={gif} className="mx-auto" alt='DevFest Save The Date Gif'></img>
         </div>
         <div className='mx-auto text-center sm:my-5 w-11/12'>
-            <h1>DevFest YOUR CITY <br/>
+            <h1>DevFest {process.env.REACT_APP_CHAPTER_NAME} <br/>
             </h1>
-            <h3 className='text-Green500 font-semibold animate-pulse'>Date: Your Date</h3>
-            <p className='sm:px-5 sm:text-lg'>DevFest YOUR CITY is a community-run one-day conference aimed to bring technologists, developers, students, tech companies, and speakers together in one location to learn, discuss and experiment with technology.</p>
+            <h3 className='text-Green500 font-semibold animate-pulse'>Date: {process.env.REACT_APP_DEVFEST_DATE}</h3>
+            <p className='sm:px-5 sm:text-lg'>DevFest {process.env.REACT_APP_CHAPTER_NAME} is a community-run one-day conference aimed to bring technologists, developers, students, tech companies, and speakers together in one location to learn, discuss and experiment with technology.</p>
             
             <div className='my-10'>
                     <a href="https://gdg.community.dev/events/details/google-gdg-baton-rouge-presents-devfest-baton-rouge-2022/" className='text-white bg-Red500 hover:text-white rounded-full text-md px-6 py-4'>Register</a> 
--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -8,18 +8,18 @@ const Sponsors = () => (
     <div className='w-11/12 mx-auto my-10 grid grid-cols-1 lg:grid-cols-2'>
         <div className='px-5 my-5'>
             <div className='aspect-w-16 aspect-h-9'>
-                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQqhwzM5un7HK-9ZAQ3-3J9ofd1tlquvzz7fkVWpiehdelDEgln037DUlUlLKThYkTBDGXvhmxnm5Ru/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen={true} mozallowfullscreen='true' webkitallowfullscreen='true' title='DevFest Sponsor Packages - GDG YOUR CITY'></iframe>
+                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQqhwzM5un7HK-9ZAQ3-3J9ofd1tlquvzz7fkVWpiehdelDEgln037DUlUlLKThYkTBDGXvhmxnm5Ru/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen={true} mozallowfullscreen='true' webkitallowfullscreen='true' title='DevFest Sponsor Packages - GDG {process.env.REACT_APP_CHAPTER_NAME}'></iframe>
             </div>
         </div>
         <div className='px-5 my-5'>
-            <h2>Why Sponsor DevFest YOUR CITY?</h2>
-            <p className='my-10 text-lg'><span className='font-semibold'>Technical Recruiting:</span> DevFest YOUR CITY is a great opportunity to meet emerging talent in the YOUR CITY tech industry.
+            <h2>Why Sponsor DevFest {process.env.REACT_APP_CHAPTER_NAME}?</h2>
+            <p className='my-10 text-lg'><span className='font-semibold'>Technical Recruiting:</span> DevFest {process.env.REACT_APP_CHAPTER_NAME} is a great opportunity to meet emerging talent in the {process.env.REACT_APP_CHAPTER_NAME} tech industry.
             </p>
             <p className='my-10 text-lg'><span className='font-semibold'>Brand Recognition/Product Exposure:</span> Expand your brand presence and expose attendees, partners, and press to your company, product, or API.
             </p>
-            <p className='my-10 text-lg'><span className='font-semibold'>Support the YOUR CITY Tech Community:</span> We are excited to be a part of the growing tech community in YOUR CITY. Join us!
+            <p className='my-10 text-lg'><span className='font-semibold'>Support the {process.env.REACT_APP_CHAPTER_NAME} Tech Community:</span> We are excited to be a part of the growing tech community in {process.env.REACT_APP_CHAPTER_NAME}. Join us!
             </p>
-            <a href='mailto:gdg.batonrouge@gmail.com' className='text-white bg-Green500 hover:text-white rounded-full text-md px-6 py-4 my-5'>Become a Sponsor</a>
+            <a href={'mailto:' + process.env.REACT_APP_DEVFEST_EMAIL} className='text-white bg-Green500 hover:text-white rounded-full text-md px-6 py-4 my-5'>Become a Sponsor</a>
             <a href='https://drive.google.com/file/d/1pjvZxFx67WWNqYvubWen6lYl5NIZS0DV/view?usp=sharing' className='block font-semibold hover:text-Blue500 my-10'>Download Sponsor Packages</a>
         </div>
         
