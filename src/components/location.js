ab0a7f348d3bd9e457891a21755a3112fa63b57a
Gil Creque <gilcreque@gmail.com>
2023-07-31 12:52:07 +0000

    Updated Assets

--- src/components/location.js
+++ src/components/location.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import VenueMap from '../assets/2023/maps.seminolestate.edu.png';
-import VenueImage from "../assets/2023/up.jpeg";
+import VenueMap from '../assets/2023/sponsors/maps.seminolestate.edu.png';
+import VenueImage from "../assets/2023/sponsors/up.jpeg";
 
 const LocationSection = () => (
   <section className='my-20'>
